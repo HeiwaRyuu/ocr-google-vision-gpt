b5deb2d31532156cb46ab0de3858edcc254d2ccf
clib <vitor.carvalho.ufu@gmail.com>
2024-07-14 02:53:23 -0300

    google document ai (not in use | worse results)

---
+++ ocr_google_cloud_document_ai.py
@@ -0,0 +1,149 @@
+from google.api_core.client_options import ClientOptions
+from google.cloud import documentai  # type: ignore
+import os
+
+from typing import Optional
+
+from google.api_core.client_options import ClientOptions
+from google.cloud import documentai  # type: ignore
+
+# TODO(developer): Uncomment these variables before running the sample.
+# project_id = "YOUR_PROJECT_ID"
+# location = "YOUR_PROCESSOR_LOCATION" # Format is "us" or "eu"
+# processor_id = "YOUR_PROCESSOR_ID" # Create processor before running sample
+# file_path = "/path/to/local/pdf"
+# mime_type = "application/pdf" # Refer to https://cloud.google.com/document-ai/docs/file-types for supported file types
+# field_mask = "text,entities,pages.pageNumber"  # Optional. The fields to return in the Document object.
+# processor_version_id = "YOUR_PROCESSOR_VERSION_ID" # Optional. Processor version to use
+
+
+def quickstart(
+    project_id: str,
+    location: str,
+    file_path: str,
+    processor_display_name: str = "My Processor",
+):
+    # You must set the `api_endpoint`if you use a location other than "us".
+    opts = ClientOptions(api_endpoint=f"{location}-documentai.googleapis.com")
+
+    client = documentai.DocumentProcessorServiceClient(client_options=opts)
+
+    # The full resource name of the location, e.g.:
+    # `projects/{project_id}/locations/{location}`
+    parent = client.common_location_path(project_id, location)
+
+    # Create a Processor
+    processor = client.create_processor(
+        parent=parent,
+        processor=documentai.Processor(
+            type_="OCR_PROCESSOR",  # Refer to https://cloud.google.com/document-ai/docs/create-processor for how to get available processor types
+            display_name=processor_display_name
+        ),
+    )
+
+    # Print the processor information
+    print(f"Processor Name: {processor.name}")
+
+    # Read the file into memory
+    with open(file_path, "rb") as image:
+        image_content = image.read()
+
+    # Load binary data
+    raw_document = documentai.RawDocument(
+        content=image_content,
+        mime_type="application/pdf",  # Refer to https://cloud.google.com/document-ai/docs/file-types for supported file types
+    )
+
+    # Configure the process request
+    # `processor.name` is the full resource name of the processor, e.g.:
+    # `projects/{project_id}/locations/{location}/processors/{processor_id}`
+    request = documentai.ProcessRequest(name=processor.name, raw_document=raw_document)
+
+    result = client.process_document(request=request)
+
+    # For a full list of `Document` object attributes, reference this page:
+    # https://cloud.google.com/document-ai/docs/reference/rest/v1/Document
+    document = result.document
+
+    return document.text
+
+
+def ocr_document(
+    project_id: str,
+    location: str,
+    processor_id: str,
+    file_path: str,
+    mime_type: str,
+    field_mask: Optional[str] = None,
+    processor_version_id: Optional[str] = None,
+) -> None:
+    # You must set the `api_endpoint` if you use a location other than "us".
+    opts = ClientOptions(api_endpoint=f"{location}-documentai.googleapis.com")
+
+    client = documentai.DocumentProcessorServiceClient(client_options=opts)
+
+    if processor_version_id:
+        # The full resource name of the processor version, e.g.:
+        # `projects/{project_id}/locations/{location}/processors/{processor_id}/processorVersions/{processor_version_id}`
+        name = client.processor_version_path(
+            project_id, location, processor_id, processor_version_id
+        )
+    else:
+        # The full resource name of the processor, e.g.:
+        # `projects/{project_id}/locations/{location}/processors/{processor_id}`
+        name = client.processor_path(project_id, location, processor_id)
+
+    # Read the file into memory
+    with open(file_path, "rb") as image:
+        image_content = image.read()
+
+    # Load binary data
+    raw_document = documentai.RawDocument(content=image_content, mime_type=mime_type)
+
+    # For more information: https://cloud.google.com/document-ai/docs/reference/rest/v1/ProcessOptions
+    # Optional: Additional configurations for processing.
+    process_options = documentai.ProcessOptions(
+        # # Process only specific pages
+        # individual_page_selector=documentai.ProcessOptions.IndividualPageSelector(
+        #     pages=[1]
+        # ),
+        # # Hint to specific language
+        # ocr_config=documentai.OcrConfig(
+        #     hints=documentai.OcrConfig.Hints(
+        #         language_hints=["pt"]
+        #     )
+        # ),
+    )
+
+    # Configure the process request
+    request = documentai.ProcessRequest(
+        name=name,
+        raw_document=raw_document,
+        field_mask=field_mask,
+        process_options=process_options,
+    )
+
+    result = client.process_document(request=request)
+
+    # For a full list of `Document` object attributes, reference this page:
+    # https://cloud.google.com/document-ai/docs/reference/rest/v1/Document
+    document = result.document
+
+    return document.text
+
+
+def main():
+    project_id = "ocr-automation-429401"
+    location = "us"  # Format is "us" or "eu"
+    processor_id = "8d34eda77a56bb3a"
+    filename = "joao-teixeira-alves-neto-prontuario.pdf"
+    file_path = os.getcwd() + f"/images/{filename}"
+    mime_type = "application/pdf"
+    processor_display_name = "ocr-processor-document-ai" # Must be unique per project, e.g.: "My Processor"
+
+    ## RUN QUICKSTART TO CREATE A PROCESSOR IF YOU DONT HAVE ONE
+    # quickstart(project_id, location, file_path, processor_display_name)
+    ocr_document(project_id, location, processor_id, file_path, mime_type)
+
+if __name__ == "__main__":
+    main()
